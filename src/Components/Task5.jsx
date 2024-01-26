2f9a48141628f7b82a5b474dc2a95adf08f40675
vaishnavi185 <vishu032004@gmail.com>
2024-01-26 22:30:14 +0530

    task5

---
+++ src/Components/Task5.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+function Task5(props) {
+    
+    // const slidbar = () => (
+    //     <ul>
+    //         {props.posts.map((post) => (
+    //             <li key={post.id}>
+    //                 {post.title}
+    //             </li>
+    //         ))}
+    //     </ul>
+    // );
+
+    const content = props.posts.map((post) => (
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            
+            <p>{post.content}</p>
+            <hr></hr>
+        </div>
+    ));
+
+    return (
+        <div>
+           
+        
+            {content}
+        </div>
+    );
+}
+
+
+
+export default function App() {
+    const posts = [
+        { id: "1", title: "hello world", content: "hello my name is tanyy" },
+        { id: "2", title: "java", content: "it is a good language" },
+        { id: "3", title: "python", content: "it helps in machine learning" },
+    ];
+    return <Task5 posts={posts} />;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Task1 from './Components/task1.jsx'
 import Task2 from './Components/Task2.jsx'
 import Tsk3 from './Components/Tsk3.jsx'
 import Tsk4 from './Components/Tsk4.jsx'
+import App from './Components/Task5.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 <Task1 name="palak " hobby="i love singing"></Task1>
 <Task2 message="it is danger"/> */}
 {/* <Tsk3></Tsk3> */}
-<Tsk4></Tsk4>
+{/* <<Tsk4></Tsk4> */}
+<App></App>
   </React.StrictMode>,
 )
